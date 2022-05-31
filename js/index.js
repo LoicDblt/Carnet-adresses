b4763dc75f8defadeda0cafc0e6f560ddce8305b
Loïc DIEBOLT <loic@dblt.fr>
2022-05-31 17:51:51 +0200

    Initialisation du dépôt

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// N'active pas le service worker sur Firefox bureau -> problème de perfs
+if (
+	"serviceWorker" in navigator &&
+	(window.navigator.userAgent.toLowerCase().indexOf("firefox") === -1 ||
+	window.navigator.userAgent.toLowerCase().indexOf("mobile") > -1)
+){
+	navigator.serviceWorker.register("../service_worker.js")
+	.then({})
+	.catch(function(erreur){
+		console.log("Service worker - enregistrement echoué :", erreur);
+	})
+}
